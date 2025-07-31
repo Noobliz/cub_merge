9114ca2902ead0411e8acffe51ff269e6a228926
Lise Guiet <lguiet@2A5.42angouleme.fr>
2025-07-31 11:25:56 +0200

    merging

---
+++ srcs/main.c
@@ -0,0 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguiet <lguiet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 12:56:22 by thgaugai          #+#    #+#             */
+/*   Updated: 2025/07/31 11:22:22 by lguiet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+#include "../includes/parsing.h"
+
+void    init_textures(t_data *data, t_param param)
+{
+    data->ea = param.ea_path;
+    data->no = param.no_path;
+    data->so = param.so_path;
+    data->we = param.we_path;
+}
+
+// int	counter_line(char *file)
+// {
+// 	int		fd;
+// 	char	*line;
+// 	int		counter;
+
+// 	counter = 0;
+// 	fd = open(file, O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		printf("Error: File descriptor is invalid!");
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	line = get_next_line(fd);
+// 	while (line != NULL)
+// 	{
+// 		counter++;
+// 		free(line);
+// 		line = NULL;
+// 		line = get_next_line(fd);
+// 	}
+// 	close(fd);
+// 	return (counter);
+// }
+
+// char	**map_init(char *file)
+// {
+// 	char	**map;
+// 	char	*line;
+// 	int		i;
+// 	int		fd;
+// 	int		count;
+
+// 	count = counter_line(file);
+// 	i = 0;
+// 	fd = open(file, O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		ft_printf("Error: Cannot open file!\n");
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	map = malloc(sizeof(char *) * (count + 1));
+// 	if (!map)
+// 	{
+// 		ft_printf("Error: Memomy allocation failed!\n");
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	line = get_next_line(fd);
+// 	while (line != NULL)
+// 	{
+// 		map[i] = ft_strdup(line);
+// 		if (!map[i])
+// 		{
+// 			ft_printf("Error: Memomy allocation failed!\n");
+// 			ft_free_map(map);
+// 		}
+// 		free(line);
+// 		line = NULL;
+// 		i++;
+// 		line = get_next_line(fd);
+// 	}
+// 	map[i] = NULL;
+// 	close(fd);
+// 	return (map);
+// }
+
+int guardian(t_data *data, t_param *param, int argc, char **argv)
+{
+    char **map_rect;
+    map_rect = NULL;
+    if (argc != 2)
+    {
+        write(2, "Error\nenter an infile .cub\n", 28);
+        return (0);
+    }
+    map_is_valid(&map_rect, argv[1], param);
+    if (!map_rect)
+        return (0);
+    data->map = map_rect;
+    init_textures(data, *param);
+    if (!is_color(param->f_color_str, data->f)
+    || !is_color(param->c_color_str, data->c) 
+    || !check_all_textures(data))
+    {
+        free_map(data->map);
+        free_param(param);
+        return (0);
+    }
+    return (1);
+}
+
+int	main(int ac, char **av)
+{
+	// char	**map;
+	// (void)ac;
+
+	// map = map_init(av[1]);
+	t_param param;
+    t_data data;
+	
+	data.map = NULL;
+    if (!guardian(&data, &param, ac, av))
+        return (1);
+
+    print_map(data.map);
+	game_engine(data.map);
+	free_map(data.map);
+    free_param(data.param);
+	return (0);
+}
