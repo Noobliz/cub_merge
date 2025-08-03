5b7b23ee02c124b7693c7f2be21f04228d76cb15
Thomas Gaugain <thgaugai@1A5.42angouleme.fr>
2025-08-03 15:47:47 +0200

    norm main.c

--- main.c
+++ main.c
@@ -3,39 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
+/*   By: thgaugai <thgaugai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 12:56:22 by thgaugai          #+#    #+#             */
-/*   Updated: 2025/08/01 18:12:49 by thomas           ###   ########.fr       */
+/*   Updated: 2025/08/03 15:46:14 by thgaugai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/cub3d.h"
 
-int guardian(t_data *data, t_param *param, int argc, char **argv)
+int	guardian(t_data *data, t_param *param, int argc, char **argv)
 {
-    char **map_rect;
-    map_rect = NULL;
-    if (argc != 2)
-    {
-        write(2, "Error\nenter an infile .cub\n", 28);
-        return (0);
-    }
-    data->param = param;
-    map_is_valid(&map_rect, argv[1], param);
-    if (!map_rect)
-        return (0);
-    data->map = map_rect;
-    init_textures(data, *param);
-    if (!is_color(param->f_color_str, data->f)
-    || !is_color(param->c_color_str, data->c) 
-    || !check_all_textures(data))
-    {
-        free_map(data->map);
-        free_param(param);
-        return (0);
-    }
-    return (1);
+	char	**map_rect;
+
+	map_rect = NULL;
+	if (argc != 2)
+	{
+		write(2, "Error\nenter an infile .cub\n", 28);
+		return (0);
+	}
+	data->param = param;
+	map_is_valid(&map_rect, argv[1], param);
+	if (!map_rect)
+		return (0);
+	data->map = map_rect;
+	init_textures(data, *param);
+	if (!is_color(param->f_color_str, data->f) || !is_color(param->c_color_str,
+			data->c) || !check_all_textures(data))
+	{
+		free_map(data->map);
+		free_param(param);
+		return (0);
+	}
+	return (1);
 }
 
 void	game_engine(t_data *data)
@@ -50,15 +50,15 @@ void	game_engine(t_data *data)
 
 int	main(int ac, char **av)
 {
-	t_param     param;
-    t_data      data;
+	t_param	param;
+	t_data	data;
 
 	data.map = NULL;
-    if (!guardian(&data, &param, ac, av))
-        return (1);
-    print_map(data.map);
+	if (!guardian(&data, &param, ac, av))
+		return (1);
+	print_map(data.map);
 	game_engine(&data);
 	free_map(data.map);
-    free_param(data.param);
+	free_param(data.param);
 	return (0);
 }
